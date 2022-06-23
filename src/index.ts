bcb442f7540a3ccad3f84a87a239af3e38b2eaa4
lelderbe <fvdc@ya.ru>
2022-06-23 18:13:10 +0300

    feat: add test robo drawing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import WebSocket, { WebSocketServer } from 'ws';
+import { drawSquare } from './square';
 
 const wss = new WebSocketServer({
 	port: 8080,
@@ -27,7 +28,15 @@ wss.on('connection', (ws) => {
 	console.log('got connection');
 	ws.on('message', (data) => {
 		console.log('received:', data);
-		console.log(data.toString());
+		const message = data.toString();
+		console.log(message);
+		const [cmd, ...rest] = message.split(' ');
+		console.log('cmd:', cmd, 'rest:', rest);
+		switch (cmd) {
+			case 'draw_square':
+				drawSquare(rest);
+				break;
+		}
 	});
 });
 
